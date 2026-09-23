eac14423ae8d19b1906c50c7d7235d108fe7ecd4
Silent Coder <satyam.singh16855@sakec.ac.in>
2026-09-23 11:32:26 +0530

    style: polish UI layout, unify typography, add tab favicon and toasts

--- support-crm/src/main/java/com/support/crm/SupportCrmApplication.java
+++ support-crm/src/main/java/com/support/crm/SupportCrmApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class 		SupportCrmApplication {
+public class SupportCrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportCrmApplication.class, args);
